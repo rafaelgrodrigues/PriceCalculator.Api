805043868ea4483afd7a3fbec05004ada01749e6
Rafael Gomes <gomes.rafael1992@hotmail.com>
2025-05-20 02:21:37 +0100

    Validations Added

--- src/PriceCalculator.Api/Program.cs
+++ src/PriceCalculator.Api/Program.cs
@@ -6,6 +6,7 @@ using PriceCalculator.Application.Dtos;
 using PriceCalculator.Application.Interfaces;
 using PriceCalculator.Application.Profiles;
 using PriceCalculator.Application.Resquests;
+using PriceCalculator.Application.Results;
 using System.Runtime.CompilerServices;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -38,13 +39,13 @@ app.MapPost("/Prices", async Task<Results<Created<PriceDto>, InternalServerError
     ICalculationService service
     ) =>
 {
-    HttpActionContext
     var result = await service.Calculate(request);
 
     if (result.IsSuccess)
         return TypedResults.Created(string.Empty, result.Data);
 
     return TypedResults.InternalServerError();
-}).AddEndpointFilter<PostPriceFilter>();
+}).AddEndpointFilter<PostPriceFilter>()
+  .Produces(422, typeof(IEnumerable<Error>));
 
 app.Run();
--- src/PriceCalculator.Application/Resquests/PriceRequest.cs
+++ src/PriceCalculator.Application/Resquests/PriceRequest.cs
@@ -1,10 +1,15 @@
-﻿namespace PriceCalculator.Application.Resquests;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PriceCalculator.Application.Resquests;
 
 public class PriceRequest
 {
+    [RegularExpression("^[0-9]*\\.?[0-9]+$", ErrorMessage = "Please enter valid Net Value")]
     public decimal? Net { get; set; }
     public decimal? Gross { get; set; }
     public decimal? VatValue { get; set; }
+
+    [AllowedValues([10, 15, 20])]
     public int VatPercentage { get; set; }
     
 }
